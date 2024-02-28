0054248adfe661776f828679d332237a05701196
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2024-02-28 11:19:08 +0100

    ADDED versioning of styles REMOVED blocksy has_action logic

--- eeb-templates.php
+++ eeb-templates.php
@@ -20,14 +20,26 @@ include EEB_TEMPLATE_PLUGIN_DIR . 'classes/magazin-helper.php';
 
 function run_eeb_plugin()
 {
-    //TODO : Add versioning of styles
-    wp_enqueue_style('eeb-general-view', plugin_dir_url(__FILE__) . '/assets/css/general-view.css', array(), '1.0');
-    wp_enqueue_style('eeb-person-view', plugin_dir_url(__FILE__) . '/assets/css/person-view.css', array(), '1.0');
-    wp_enqueue_style('eeb-magazine-view', plugin_dir_url(__FILE__) . '/assets/css/magazine-view.css', array(), '1.0');
-    wp_enqueue_style('eeb-organisation-list-view', plugin_dir_url(__FILE__) . '/assets/css/organisationen-list-view.css', array(), '1.0');
-    wp_enqueue_style('eeb-carousel', plugin_dir_url(__FILE__) . '/assets/css/carousel.css', array(), '1.0');
+    $enqueues = [
+        'styles' => [
+            'eeb-general-view' => '/assets/css/general-view.css',
+            'eeb-person-view' => '/assets/css/person-view.css',
+            'eeb-magazine-view' => '/assets/css/magazine-view.css',
+            'eeb-organisation-list-view' => '/assets/css/organisationen-list-view.css',
+            'eeb-carousel' => '/assets/css/carousel.css'
 
+        ],
+    ];
+    if (!function_exists('get_plugin_data')) {
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
 
+    $plugin_data = get_plugin_data(__FILE__);
+
+    $plugin_version = $plugin_data['Version'];
+    foreach ($enqueues['styles'] as $style_handle => $style_path) {
+        wp_enqueue_style($style_handle, plugin_dir_url(__FILE__) . $style_path, [], $plugin_version . '.' . filemtime(__DIR__ . $style_path));
+    }
     //Personen Views
     add_shortcode('eeb-person-view', 'display_person_view');
     add_shortcode('eeb-ansprechpartner-view', 'display_ansprechpartner');
@@ -38,26 +50,11 @@ function run_eeb_plugin()
     add_shortcode('eeb-inhaltsverzeichnis', 'display_magazin_iframe');
     add_shortcode('eeb-vergangene-ausgaben', 'display_older_publications');
 
+    //Blocksy Hooks
+    add_action('blocksy:single:content:bottom', array('Eeb_General_Helper', 'display_logo_postfix'));
+    add_action('blocksy:header:after', array('Eeb_General_Helper', 'display_magazin_button'));
 
-//Blocksy Hooks
-
-//TODO  add check has_action('blocksy:single:content:bottom') && has_action('blocksy:header:after')
-    if (true) {
-        add_action('blocksy:single:content:bottom', array('Eeb_General_Helper', 'display_logo_postfix'));
-        add_action('blocksy:header:after', array('Eeb_General_Helper', 'display_magazin_button'));
-
-    } else {
-        add_action('notice-error', 'error_blocksy_hooks_missing');
-    }
     add_action('save_post', 'alter_ausgabe_content', 10, 3);
-}
-
-function error_blocksy_hooks_missing()
-{
-    ?>
-    <strong> EEB Plugin ERROR: </strong>
-    <p>Blocksy hooks not found this may cause some features to not work properly</p>
-    <?php
 }
 
 run_eeb_plugin();
